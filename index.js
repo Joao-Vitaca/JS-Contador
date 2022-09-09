de39106435f33daa9b5b65a126dec869a6e9bce1
Sysop-Developer <victornovato123@Gmail.com>
2022-09-09 15:07:55 -0300

    .

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-
-var final = Number(document.getElementById('numeroFinal').value)
-var pular = document.getElementById('numeroPular')
-var msg = document.getElementById('msg')
-var btn = document.getElementById('btn')
-
-btn.addEventListener("click", Clicou)
-
 function Clicou(){
-    var inicial = Number(document.getElementById('numeroInicial').value)
+    let inicial = Number(document.getElementById('numeroInicial').value)
+    let final = Number(document.getElementById('numeroFinal').value)
+    let pular = Number(document.getElementById('numeroPular').value)
+    let msg = document.getElementById('msg')
+    let btn = document.getElementById('btn')
     msg.innerHTML = ''
-    for(inicial; inicial <= final; inicial++){
-        msg.append('oi')
-        console.log(inicial)
+
+    if(inicial == 0 || final == 0 || pular < 0){
+        window.alert('Erro')
+    }else{
+        if(pular == 0){
+            pular = 1
+        }
+        msg.innerHTML = `Contando: `
+        if(inicial < final ){
+            for(inicial; inicial < final; inicial+=pular){
+            msg.innerHTML += `${inicial}\u{1F448} `  
+            }
+        }else{
+            for(inicial; inicial > final; inicial-=pular){
+                msg.innerHTML += `${inicial}\u{1F448} `  
+                }
+        }
     }
 }
